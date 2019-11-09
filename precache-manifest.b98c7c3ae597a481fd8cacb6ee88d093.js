1a5dceaae715ad3b445a89e6e529d4435bb949d2
IryArkhy <37odri@gmail.com>
2019-11-09 13:23:59 +0200

    Updates

---
+++ precache-manifest.b98c7c3ae597a481fd8cacb6ee88d093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0f9d689443a3caeb937173a293230d",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "e98223d19f90e918077d",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f237679e3e3e5a27ad9f",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.43d178ea.chunk.css"
+  },
+  {
+    "revision": "e98223d19f90e918077d",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.15adb9cd.chunk.js"
+  },
+  {
+    "revision": "f237679e3e3e5a27ad9f",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.b2edef2f.chunk.js"
+  },
+  {
+    "revision": "4456fcf01a6249bbb0da",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.b10a97a6.js"
+  }
+]);
